10757ba5eaf4fc07c9cbcc861cf85dacf4a6fa77
vanplus <60313789+vanplus@users.noreply.github.com>
2023-09-17 15:25:08 +0800

    Update install-bottom.sh

--- install-bottom.sh
+++ install-bottom.sh
@@ -13,6 +13,11 @@ release_info=$(curl -sSL "${RELEASES_URL}")
 # 提取最新 Release 的 tag_name（版本号）
 tag_name=$(echo "$release_info" | grep -o '"tag_name": "[^"]*' | sed 's/"tag_name": "//')
 
+if [ -z "${tag_name}" ]; then
+  echo "获取到的 tag_name 为空，无法下载，release_info: ${release_info}"
+  exit 1
+fi
+
 # 构建文件下载 URL
 file_url="https://github.com/${OWNER}/${REPO}/releases/latest/download/bottom_${tag_name}_amd64.deb"
 
