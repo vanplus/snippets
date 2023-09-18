42871637ea386d252a4822b79c4abb056e24cfb9
vanplus <60313789+vanplus@users.noreply.github.com>
2023-09-19 04:33:35 +0800

    Update create-systemd-service.sh

--- create-systemd-service.sh
+++ create-systemd-service.sh
@@ -5,21 +5,61 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-service_name="$1"
-command_to_run="$2"
-restart_option="$3"
+SERVICE_NAME="$1"
+COMMAND_TO_RUN="$2"
+RESTART_OPTION="$3"
+
+# 检查是否运行在 Alpine Linux 上
+if [ -f /etc/alpine-release ]; then
+    # 创建 init.d 服务文件
+    SERVICE_FILE="/etc/init.d/$SERVICE_NAME"
+    
+    cat <<EOF > "$SERVICE_FILE"
+#!/sbin/openrc-run
+
+name="$SERVICE_NAME"
+description="Custom Service: $SERVICE_NAME"
+
+command="$COMMAND_TO_RUN"
+
+command_args=""
+
+pidfile="/var/run/\$RC_SVCNAME.pid"
+
+start() {
+    ebegin "Starting \$name"
+    start-stop-daemon --start --exec \$command -- \$command_args
+    eend $?
+}
+
+stop() {
+    ebegin "Stopping \$name"
+    start-stop-daemon --stop --exec \$command
+    eend $?
+}
+EOF
+
+    # 设置服务文件可执行权限
+    chmod +x "$SERVICE_FILE"
+
+    # 添加服务到启动项
+    rc-update add "$SERVICE_NAME" default
+
+    echo "Init.d service '$SERVICE_NAME' created and enabled."
+    exit 0
+fi
 
 # 创建 Systemd 服务单元文件
-service_file="/etc/systemd/system/$service_name.service"
+service_file="/etc/systemd/system/$SERVICE_NAME.service"
 
 cat <<EOF > "$service_file"
 [Unit]
-Description=$service_name
+Description=$SERVICE_NAME
 After=network.target
 
 [Service]
-ExecStart="$command_to_run"
-Restart=$restart_option
+ExecStart="$COMMAND_TO_RUN"
+Restart=$RESTART_OPTION
 User=$(whoami)
 
 [Install]
@@ -30,10 +70,10 @@ EOF
 systemctl daemon-reload
 
 # 启用服务开机自启动
-systemctl enable "$service_name"
+systemctl enable "$SERVICE_NAME"
 
 # 启动服务
-systemctl start "$service_name"
+systemctl start "$SERVICE_NAME"
 
 # 检查服务状态
-systemctl status "$service_name"
+systemctl status "$SERVICE_NAME"
