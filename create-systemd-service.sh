b8ac6df02803cea3cab4592a0031ff584c0120ad
vanplus <60313789+vanplus@users.noreply.github.com>
2023-09-19 03:39:10 +0800

    Update create-systemd-service.sh

--- create-systemd-service.sh
+++ create-systemd-service.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <service-name> <command-to-run>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <service-name> <command-to-run> <restart-option>"
     exit 1
 fi
 
 service_name="$1"
 command_to_run="$2"
+restart_option="$3"
 
 # 创建 Systemd 服务单元文件
 service_file="/etc/systemd/system/$service_name.service"
@@ -18,7 +19,7 @@ After=network.target
 
 [Service]
 ExecStart=$command_to_run
-Restart=always
+Restart=$restart_option
 User=$(whoami)
 
 [Install]
