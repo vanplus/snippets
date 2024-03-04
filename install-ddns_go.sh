fa6671d7837e6908b308e6654c0c2f77bbc8741f
vanplus <60313789+vanplus@users.noreply.github.com>
2024-03-04 23:39:03 +0800

    Update install-ddns_go.sh

--- install-ddns_go.sh
+++ install-ddns_go.sh
@@ -2,7 +2,11 @@
 
 # 解析参数
 
+# 默认参数
 ddns_go_args="-f 10 -cacheTimes 180 -noweb"
+root_domain=""
+ip_gettype="url"
+
 # 查找 '--' 参数的位置
 separator_pos=0
 for arg in "$@"; do
@@ -18,6 +22,23 @@ if [ "$separator_pos" -lt "$#" ]; then
     ddns_go_args=("${@:separator_pos+1}")
     ddns_go_args="${ddns_go_args[@]}"
 fi
+
+# 解析命令行选项
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --root-domain)
+            root_domain="$2"
+            shift 2
+            ;;
+        --ip-gettype)
+            ip_gettype="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
 # 如果 ddns-go 已经安装，则提示用户卸载
 
@@ -45,9 +66,34 @@ CONFIG_FILE_NAME="${INSTALL_DIR}/config.yaml"
 
 # 生成配置文件
 
-# 函数：获取当前网络接口列表
-list_net_interfaces() {
-    ip -o link show | awk -F': ' '{print $2}' | grep -v lo
+# 获取当前网络接口列表
+get_interfaces() {
+    # 获取所有网络接口的名称，排除lo
+    interfaces=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo)
+
+    # 创建一个空数组来存储接口名称和IP地址
+    interface_info=()
+
+    # 根据参数进行不同的输出
+    if [ "$1" == "-4" ]; then
+        # 输出每个接口的名称和 IPv4 地址
+        for interface in $interfaces; do
+            ipv4=$(ip -o -4 addr show dev $interface | awk 'BEGIN { ORS=", "; } {print $4}' | sed 's/, $/\n/')
+            interface_info+=("$interface ($ipv4)")
+        done
+    elif [ "$1" == "-6" ]; then
+        # 输出每个接口的名称和 IPv6 地址
+        for interface in $interfaces; do
+            ipv6=$(ip -o -6 addr show dev $interface | awk 'BEGIN { ORS=", "; } {print $4}' | sed 's/, $/\n/')
+            interface_info+=("$interface ($ipv6)")
+        done
+    else
+        echo "无效的参数。使用 -4 获取 IPv4 地址，使用 -6 获取 IPv6 地址。"
+        return 1
+    fi
+
+    # 返回接口信息数组
+    printf "%s\n" "${interface_info[@]}"
 }
 
 # 读取用户输入，支持默认值
@@ -72,46 +118,69 @@ read_required() {
     done
 }
 
+generate_random_hex() {
+    dd if=/dev/urandom bs=4 count=1 2>/dev/null | xxd -p -c 8 | tr -d '\n'
+}
+
+generate_random_password() {
+    head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12
+}
+
+ipv4_domain=""
+ipv6_domain=""
+# 如果指定了 --root-domain，生成特定格式的字符串
+if [ ! -z "$root_domain" ]; then
+    hostname=$(hostname)
+    random_hex=$(generate_random_hex)
+    ipv4_domain="${hostname}.${random_hex}:${root_domain}"
+    ipv6_domain="${hostname}.v6.${random_hex}:${root_domain}"
+fi
+
 # 读取 dns.secret
 echo "Cloudflare token 可访问 https://dash.cloudflare.com/profile/api-tokens 申请，Create Token -> Edit Zone DNS (Use template)"
 dns_secret=$(read_required "请输入 Cloudflare token")
 
 # 读取 IPv4 配置
 ipv4_enable=$(read_with_default "IPv4 是否启用? (true/false)" "true")
-ipv4_gettype="url"
+ipv4_gettype=$ip_gettype
 if [ "$ipv4_enable" = "true" ]; then
-    ipv4_gettype=$(read_with_default "IPv4 获取 IP 的方式(url/netInterface/cmd)" "url")
+    ipv4_gettype=$(read_with_default "IPv4 获取 IP 的方式 (url/netInterface/cmd)" "$ipv4_gettype")
     if [ "$ipv4_gettype" = "cmd" ]; then
         ipv4_cmd=$(read_required "请输入 IPv4 获取 IP 的命令")
     elif [ "$ipv4_gettype" = "netInterface" ]; then
-        echo "可用网络接口:"
-        list_net_interfaces
-        ipv4_netinterface=$(read_required "请输入网络接口名字")
+        readarray -t interface_array < <(get_interfaces -4)
+        echo "可用网络接口："
+        for item in "${interface_array[@]}"; do
+            echo "$item"
+        done
+
+        ipv4_netinterface=$(echo "${interface_array[0]}" | awk '{print $1}')
+        ipv4_netinterface=$(read_with_default "请输入网络接口名字" "$ipv4_netinterface")
     fi
     echo "请输入 IPv4 的 domains，「使用冒号」分隔子域名和根域名，比如 www:example.cn.eu.org（支持多条域名, 使用空格分隔）："
-    read -e -ra ipv4_domains
+    read -e -ra ipv4_domains -i "$ipv4_domain"
 fi
 
 # 读取 IPv6 配置
 ipv6_enable=$(read_with_default "IPv6 是否启用? (true/false)" "false")
-ipv6_gettype="url"
+ipv6_gettype=$ip_gettype
 if [ "$ipv6_enable" = "true" ]; then
-    ipv6_gettype=$(read_with_default "IPv6 获取 IP 的方式(url/netInterface/cmd)" "url")
+    ipv6_gettype=$(read_with_default "IPv6 获取 IP 的方式 (url/netInterface/cmd)" "$ipv6_gettype")
     if [ "$ipv6_gettype" = "cmd" ]; then
         ipv6_cmd=$(read_required "请输入 IPv6 获取IP的命令")
     elif [ "$ipv6_gettype" = "netInterface" ]; then
-        echo "可用网络接口:"
-        list_net_interfaces
-        ipv6_netinterface=$(read_required "请选择一个网络接口")
+        readarray -t interface_array < <(get_interfaces -6)
+        echo "可用网络接口："
+        for item in "${interface_array[@]}"; do
+            echo "$item"
+        done
+
+        ipv6_netinterface=$(echo "${interface_array[0]}" | awk '{print $1}')
+        ipv6_netinterface=$(read_with_default "请输入网络接口名字" "$ipv6_netinterface")
     fi
     echo "请输入 IPv6 的 domains, 子域名和根域名之间用冒号分隔，比如 www:example.cn.eu.org（支持多条域名, 使用空格分隔）："
-    read -e -ra ipv6_domains
+    read -e -ra ipv6_domains -i "$ipv6_domain"
 fi
-
-# 生成随机密码
-generate_random_password() {
-    head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12
-}
 
 # 自动生成的用户名和密码
 username="admin"
@@ -209,7 +278,7 @@ ddns_go_args=$(read_with_default "输入 ddns-go 安装命令参数" "${ddns_go_
 # 如果启用 -noweb 则再次询问是否启用 web ui
 if [[ "$ddns_go_args" == *"-noweb"* ]]; then
     # 询问用户是否启用 Web UI
-    enable_web_ui=$(read_with_default "是否启用 Web UI? (y/n)" "y")
+    enable_web_ui=$(read_with_default "是否启用 Web UI? (n/y)" "n")
     if [[ "$enable_web_ui" == "y" ]]; then
         # 如果启用，则去掉 -noweb 参数
         ddns_go_args="${ddns_go_args/-noweb/}"
@@ -228,3 +297,11 @@ reset="\033[0m"
 echo -e "\n"
 echo -e "用户名: ${green}${username}${reset}"
 echo -e "密码: ${green}${password}${reset}"
+
+for domain in "${ipv4_domains[@]}"; do
+    echo -e "IPv4 域名： ${green}${domain//:/\.}${reset}"
+done
+
+for domain in "${ipv6_domains[@]}"; do
+    echo -e "IPv6 域名： ${green}${domain//:/\.}${reset}"
+done
