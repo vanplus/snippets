9deb3111ffdd68c5f1c7f5c401e90195e1d69a69
vanplus <60313789+vanplus@users.noreply.github.com>
2024-09-21 16:38:32 +0800

    Update install-btop.sh

--- install-btop.sh
+++ install-btop.sh
@@ -16,7 +16,7 @@ trap 'cleanup' EXIT
 cd "$temp_dir"
 
 # 确定最新的 release 页面
-latest_release_url=$(curl -sI https://github.com/aristocratos/btop/releases/latest | grep -i location | awk '{print $2}' | tr -d '\r')
+latest_release_url=$(curl --http1.1 -sI https://github.com/aristocratos/btop/releases/latest | grep -i location | awk '{print $2}' | tr -d '\r')
 
 # 提取版本号
 version=$(echo $latest_release_url | grep -oP "tag/\K(v.*)")
@@ -25,7 +25,7 @@ version=$(echo $latest_release_url | grep -oP "tag/\K(v.*)")
 download_url="https://github.com/aristocratos/btop/releases/download/$version/btop-x86_64-linux-musl.tbz"
 
 # 下载文件
-curl -L $download_url -o btop-x86_64-linux-musl.tbz
+curl --http1.1 -L $download_url -o btop-x86_64-linux-musl.tbz
 
 echo "Download completed: btop-x86_64-linux-musl.tbz"
 
